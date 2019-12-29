cef63cc3118eaf45c726840b9fa0e292a7a431a7
Saifi Ikmal Sinan <saifi.ikmal@gmail.com>
2019-12-30 01:19:43 +0800

    Added fallback to pin

--- ios/RNSensitiveInfo/RNSensitiveInfo.m
+++ ios/RNSensitiveInfo/RNSensitiveInfo.m
@@ -170,7 +170,8 @@ RCT_EXPORT_METHOD(getItem:(NSString *)key options:(NSDictionary *)options resolv
     
     if([RCTConvert BOOL:options[@"touchID"]]){
         LAContext *context = [[LAContext alloc] init];
-        context.localizedFallbackTitle = @"";
+        NSString *kLocalizedFallbackTitle = [RCTConvert NSString:options[@"kLocalizedFallbackTitle"]];
+        context.localizedFallbackTitle = kLocalizedFallbackTitle ? kLocalizedFallbackTitle : @"";
         context.touchIDAuthenticationAllowableReuseDuration = 1;
         
         [query setValue:context forKey:(NSString *)kSecUseAuthenticationContext];
@@ -179,8 +180,11 @@ RCT_EXPORT_METHOD(getItem:(NSString *)key options:(NSDictionary *)options resolv
         if ([RCTConvert NSString:options[@"kSecUseOperationPrompt"]] != NULL) {
             prompt = [RCTConvert NSString:options[@"kSecUseOperationPrompt"]];
         }
+
+        // If kLocalizedFallbackTitle exist, LAPolicy must allow fallback to pin also
+        NSInteger policy = kLocalizedFallbackTitle ? LAPolicyDeviceOwnerAuthentication : LAPolicyDeviceOwnerAuthenticationWithBiometrics;
         
-        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+        [context evaluatePolicy:policy
                 localizedReason:prompt
                           reply:^(BOOL success, NSError * _Nullable error) {
                               if (!success) {
